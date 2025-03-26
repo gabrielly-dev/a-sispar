071673248854317186f1663c20e87152a7e913c6
Gabrielly Barbosa Silva <gabriellybarbosa.dev@gmail.com>
2025-03-25 22:25:26 -0300

    styles: Finalizando o formulário da página de Solicitação de Reembolso

--- src/components/solicitacao/Solicitacao.jsx
+++ src/components/solicitacao/Solicitacao.jsx
@@ -167,79 +167,81 @@ function Solicitacao () {
                     <section className={styles.seccaoTabela}>
 
                         <table>
-                                <tr>
-                                    <th></th>
-                                    <th>Colaborador(a)</th>
-                                    <th>Empresa</th>
-                                    <th>Nº Prest.</th>
-                                    <th>Data</th>
-                                    <th>Motivo</th>
-                                    <th>Tipo de despesa</th>
-                                    <th>Ctr. Custo</th>
-                                    <th>Ord. Int.</th>
-                                    <th>Div.</th>
-                                    <th>PEP</th>
-                                    <th>Moeda</th>
-                                    <th>Dist. Km</th>
-                                    <th>Val. Km</th>
-                                    <th>Val. Faturado</th>
-                                    <th>Despesa</th>
+                            
+                            <tr>
+                                <th></th>
+                                <th>Colaborador(a)</th>
+                                <th>Empresa</th>
+                                <th>Nº Prest.</th>
+                                <th>Data</th>
+                                <th>Motivo</th>
+                                <th>Tipo de despesa</th>
+                                <th>Ctr. Custo</th>
+                                <th>Ord. Int.</th>
+                                <th>Div.</th>
+                                <th>PEP</th>
+                                <th>Moeda</th>
+                                <th>Dist. Km</th>
+                                <th>Val. Km</th>
+                                <th>Val. Faturado</th>
+                                <th>Despesa</th>
                                 </tr>
-                                <tr>
-                                    <td><img src={Lixeira} alt="" /></td>
-                                    <td>Maria Anders</td>
-                                    <td>WSS001</td>
-                                    <td>329456</td>
-                                    <td>08/01/2025</td>
-                                    <td><img src={Motivo} alt="" /></td>
-                                    <td>Desp. de viagem administrativa</td>
-                                    <td>1100110002 - FIN VICE-PRESIDENCIA FINANCAS MTZ</td>
-                                    <td>0003</td>
-                                    <td>002</td>
-                                    <td>001</td>
-                                    <td>BRL</td>
-                                    <td>434Km</td>
-                                    <td>0.65</td>
-                                    <td>242.10</td>
-                                    <td>40.05</td>
+                            <tr>
+                                <td><img src={Lixeira} alt="" /></td>
+                                <td>Maria Anders</td>
+                                <td>WSS001</td>
+                                <td>329456</td>
+                                <td>08/01/2025</td>
+                                <td><img src={Motivo} alt="" /></td>
+                                <td>Desp. de viagem administrativa</td>
+                                <td>1100110002 - FIN VICE-PRESIDENCIA FINANCAS MTZ</td>
+                                <td>0003</td>
+                                <td>002</td>
+                                <td>001</td>
+                                <td>BRL</td>
+                                <td>434Km</td>
+                                <td>0.65</td>
+                                <td>242.10</td>
+                                <td>40.05</td>
                                 </tr>
-                                <tr>
-                                    <td><img src={Lixeira} alt="" /></td>
-                                    <td>Vanessa Portugal</td>
-                                    <td>WSS002</td>
-                                    <td>987789</td>
-                                    <td>01/01/2025</td>
-                                    <td><img src={Motivo} alt="" /></td>
-                                    <td>Desp. de viagem administrativa</td>
-                                    <td>1100110102 - FIN CONTABILIDADE MTZ</td>
-                                    <td>0002</td>
-                                    <td>005</td>
-                                    <td>001</td>
-                                    <td>ARS</td>
-                                    <td>289Km</td>
-                                    <td>0.37</td>
-                                    <td>106.93</td>
-                                    <td>0.00</td>
-                                </tr>
-                                <tr>
-                                    <td><img src={Lixeira} alt="" /></td>
-                                    <td>Washington Klinglon</td>
-                                    <td>WSS003</td>
-                                    <td>546791</td>
-                                    <td>03/01/2025</td>
-                                    <td><img src={Motivo} alt="" /></td>
-                                    <td>Eventos de apresentação</td>
-                                    <td>1100109002 - FIN CONTROLES INTERNOS MTZ</td>
-                                    <td>0001</td>
-                                    <td>005</td>
-                                    <td>001</td>
-                                    <td>USD</td>
-                                    <td>197Km</td>
-                                    <td>0.75</td>
-                                    <td>109.75</td>
-                                    <td>29.97</td>
-                                </tr>
-                            </table>
+                            <tr>
+                                <td><img src={Lixeira} alt="" /></td>
+                                <td>Vanessa Portugal</td>
+                                <td>WSS002</td>
+                                <td>987789</td>
+                                <td>01/01/2025</td>
+                                <td><img src={Motivo} alt="" /></td>
+                                <td>Desp. de viagem administrativa</td>
+                                <td>1100110102 - FIN CONTABILIDADE MTZ</td>
+                                <td>0002</td>
+                                <td>005</td>
+                                <td>001</td>
+                                <td>ARS</td>
+                                <td>289Km</td>
+                                <td>0.37</td>
+                                <td>106.93</td>
+                                <td>0.00</td>
+                            </tr>
+                            <tr>
+                                <td><img src={Lixeira} alt="" /></td>
+                                <td>Washington Klinglon</td>
+                                <td>WSS003</td>
+                                <td>546791</td>
+                                <td>03/01/2025</td>
+                                <td><img src={Motivo} alt="" /></td>
+                                <td>Eventos de apresentação</td>
+                                <td>1100109002 - FIN CONTROLES INTERNOS MTZ</td>
+                                <td>0001</td>
+                                <td>005</td>
+                                <td>001</td>
+                                <td>USD</td>
+                                <td>197Km</td>
+                                <td>0.75</td>
+                                <td>109.75</td>
+                                <td>29.97</td>
+                            </tr>
+                                
+                        </table>
 
                     </section>
 
